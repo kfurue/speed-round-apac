35da4faea4e04d7a93e23bfb64757c5ca89eba6e
kfurue <k.furue0728@gmail.com>
2021-03-13 12:02:28 +0900

    Add Style

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
@@ -17,27 +17,46 @@ package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.R
 
 // Set of Material typography styles to start with
-val typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+
+private val appFontFamily = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = R.font.nunito_sans_bold,
+            weight = FontWeight.Bold
+        ),
+        Font(
+            resId = R.font.nunito_sans_semi_bold,
+            weight = FontWeight.SemiBold
+        ),
+        Font(
+            resId = R.font.nunito_sans_light,
+            weight = FontWeight.Light
+        )
     )
-        /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )
+
+val defaultTypography = Typography()
+val appTypography = Typography(
+    h1 = defaultTypography.h1.copy(fontFamily = appFontFamily),
+    h2 = defaultTypography.h2.copy(fontFamily = appFontFamily),
+    h3 = defaultTypography.h3.copy(fontFamily = appFontFamily),
+    h4 = defaultTypography.h4.copy(fontFamily = appFontFamily),
+    h5 = defaultTypography.h5.copy(fontFamily = appFontFamily),
+    h6 = defaultTypography.h6.copy(fontFamily = appFontFamily),
+    subtitle1 = defaultTypography.subtitle1.copy(fontFamily = appFontFamily),
+    subtitle2 = defaultTypography.subtitle2.copy(fontFamily = appFontFamily),
+    body1 = defaultTypography.body1.copy(fontFamily = appFontFamily),
+    body2 = defaultTypography.body2.copy(fontFamily = appFontFamily),
+    button = defaultTypography.button.copy(fontFamily = appFontFamily),
+    caption = defaultTypography.caption.copy(fontFamily = appFontFamily),
+    overline = defaultTypography.overline.copy(fontFamily = appFontFamily)
+)
+
